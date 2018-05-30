0f82c58db99d744162653fcb3bece0d73c67863a
arnaudbastard <39769934+arnaudbastard@users.noreply.github.com>
2018-05-30 16:54:34 +0200

    Travail du 30/05
    - Initiation JS
    - Les Variables
    - Les Tableaux
    - Les objets
    - La Concaténation
    - La documentation

---
+++ js/assets/js/03.js
@@ -0,0 +1,11 @@
+/* LA CONCATENATION */
+
+var DebutDePhrase = "Aujourd'hui ";
+var DateDuJour = new Date(); /*Nouvel élément de type date, d'où le new */
+var SuiteDePhrase = ", sont présents : ";
+var NombreDeStagiaire = 15;
+var FinDePhrase = " apprenants.<br>";
+
+/* Nous souhaitons maintenant grâce à la concaténation, afficher tout ce petit monde en un seul morceau. */
+
+document.write( DebutDePhrase + DateDuJour.getDate() + '/' + ( DateDuJour.getMonth() + 1 ) + '/' + DateDuJour.getFullYear() + SuiteDePhrase + NombreDeStagiaire + FinDePhrase );
